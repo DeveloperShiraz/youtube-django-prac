07357705d1acde1ae0cf348f40f6caac87d0c427
Mohammed Shiraz <lightningboy78@gmail.com>
2023-06-21 12:12:37 -0400

    Splitted Settings.py file into sub folders

---
+++ real_estate/settings/development.py
@@ -0,0 +1,20 @@
+from .base import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
+    }
+}
+
+# The code below this line is old.
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
