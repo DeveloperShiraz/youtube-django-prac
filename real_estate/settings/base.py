63b946952ba9bc9674877e9d72961dc1fa181b62
Mohammed Shiraz <lightningboy78@gmail.com>
2023-06-22 13:22:16 -0400

    The User Models has been pushed.

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -22,7 +22,6 @@ DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 
-
 # Application definition
 
 DJANGO_APPS = [
@@ -124,6 +123,7 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'users.User'
 
 logger = logging.getLogger(__name__)
 
